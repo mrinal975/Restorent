5976737800710ab7f61a73952f77c5e70ac1fdc6
mrinal975 <mrinalmallik1@gmail.com>
2023-03-08 20:42:37 +0600

    get item on home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,31 @@
 <template>
 <Header />
 <h1>Hello {{name}}, welcome on Home page</h1>
+
+<table border="1">
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Address</td>
+        <td>Contact</td>
+    </tr>
+    <tr v-for="item in restaurant" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.address }}</td>
+        <td>{{ item.contact }}</td>
+    </tr>
+</table>
 </template>
 <script>
 import Header from './Header.vue';
+import axios from 'axios';
 export default{
     name:'Home',
     data(){
         return {
-            name:""
+            name:"",
+            restaurant:[]
         }
     },
 
@@ -16,13 +33,17 @@ export default{
         Header
     },
 
-    mounted()
+    async mounted()
     {
         let user = JSON.parse(localStorage.getItem('user-info'));
         this.name = user.name;
         if(!user){
             this.$router.push({name:"SignUp"});
-        }
+        } 
+        let result = await axios.get("http://localhost:3000/restaurant");
+        this.restaurant = result.data;
     }
 }
 </script>
+
+<style></style>
